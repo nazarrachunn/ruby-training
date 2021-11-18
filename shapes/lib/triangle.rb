5a4b02f2a0b703aaf39ea89592b3501534b44252
nazarrachunn <nazarrachun@gmail.com>
2021-11-18 10:11:52 +0200

    morning

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
-  equilateral = :equilateral
+  equilateral = :'equilateral'
 end
 
+def triangle(a, b, c)
+  isosceles = :'isosceles'
+end
 
+def triangle(a, b, c)
+  scalene = :'scalene'
+end
 
 # Error class used in invalid_triangle_spec. No need to change this code.
 class TriangleError < StandardError
