abfa8a95ae7b9555f1f4717c490be6bff6e529bd
nazarrachunn <nazarrachun@gmail.com>
2021-11-22 23:18:26 +0200

    long day today

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -1,12 +1,20 @@
 # frozen_string_literal: true
 
 def quadrilateral(a, b, c, d)
-  sum = 360
-  return [:square, :rectangle] if a == b && b == c && c == d
-  return [:parallelogram, :rhombus] if a+b != c+d or a+d != b+d or a+d != b+c
-  return [:parallelogram, :rhombus] if ((a==b) && (c==d)) || ((a==d) && (b==d)) || ((a==d) && (b==c))
-  return [:quadrilateral]
+
+  if (a == b) && (b == c) && (c == d)
+    [:square, :rectangle]
+  elsif ((a == b) && (c == d)) || ((a == c) && (b == d)) || ((a == d) && (b == c))
+    [:parallelogram, :rhombus]
+  else
+    [:quadrilateral]
+  end
+
+
+  
 end
+  
+
 # Error class used in invalid_quadrilateral_spec. No need to change this code.
 class QuadrilateralError < StandardError
 end
