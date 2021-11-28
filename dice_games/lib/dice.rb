d17c1a9b73d633c8520d58f3ac2441b55e8152f4
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2021-11-28 18:37:57 +0200

    Dice (#44)
    * resolving
    
    * finally
    
    * next step
    
    * It's a piece of cake
    
    * start with dice
    
    * attr_reader
    
    * dice third step
    
    * end
    
    * dice end
    
    Co-authored-by: nazarrachunn <nazarrachun@gmail.com>

--- dice_games/lib/dice.rb
+++ dice_games/lib/dice.rb
@@ -2,11 +2,9 @@
 
 class Dice
     attr_reader :values
-  
-    def roll(number)
+    def roll(number = 1)
     @values = []
-     number.times {@values.push rand(6)+1 }
-
+     number.times { @values.push rand(6)+1 }
     end
 end
 
