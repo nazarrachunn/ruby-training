f7aa6a8ae72b321f8625393cc3a6183ab11cfffe
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2021-11-28 18:54:33 +0200

    dice-finish (#46)
    Co-authored-by: nazarrachunn <nazarrachun@gmail.com>

--- dice_games/lib/dice.rb
+++ dice_games/lib/dice.rb
@@ -4,7 +4,7 @@ class Dice
     attr_reader :values
     def roll(number = 1)
     @values = []
-     number.times { @values.push rand(6)+1 }
+     number.times { values.push rand(6)+1 }
     end
 end
 
