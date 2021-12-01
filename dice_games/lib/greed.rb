903594822ab76af74373e9a029ac2a3b2d4d7613
nazarrachunn <nazarrachun@gmail.com>
2021-12-01 23:48:06 +0200

    ...

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -1,22 +1,25 @@
 # frozen_string_literal: true
 
 class Greed
-    def score(number = [])
-      if number == []
+    def score(number = nil)
+      if number == nil
       raise GreedError
       end
-      number.uniq.each do |item|
+      total_score = 0
+      if (number.count(1)/3) == 1 
+        point_scored += 1000
+        3.times { number.shift }
+      end
+      number.each do |item|
         if item == 1
           return 100
         elsif item == 5
           return 50
-        elsif item >=6 
+        elsif item == 4 || 3 || 2 || 6
           return 0
-        elsif item == 4 || 3 || 2
-          return 0
-      
         end
     end
+    total_score
   end
 end
 
