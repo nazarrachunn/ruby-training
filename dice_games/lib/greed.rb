b9788df5fc4f2ba6ddddb923e8239c4e9054944f
nazarrachunn <nazarrachun@gmail.com>
2021-12-02 00:22:18 +0200

    bad way

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -6,22 +6,7 @@ class Greed
       raise GreedError
       end
       total_score = 0
-      if (number.count(1)/3) == 1 
-        point_scored += 1000
-        3.times { number.shift }
-      end
-      number.each do |item|
-        if item == 1
-          return 100
-        elsif item == 5
-          return 50
-        elsif item == 4 || 3 || 2 || 6
-          return 0
-        end
     end
-    total_score
-  end
 end
-
 class GreedError < StandardError
 end
