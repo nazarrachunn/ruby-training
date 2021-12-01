47e14172d71b3b281d4ae2397523eeaad521bf4d
nazarrachunn <nazarrachun@gmail.com>
2021-12-01 22:38:02 +0200

    17 left

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -5,12 +5,16 @@ class Greed
       if number == []
       raise GreedError
       end
-      total = 0
       number.uniq.each do |item|
         if item == 1
           return 100
         elsif item == 5
           return 50
+        elsif item >=6 
+          return 0
+        elsif item == 4 || 3 || 2
+          return 0
+      
         end
     end
   end
