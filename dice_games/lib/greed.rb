966d44a3b483ea427181839909b702e825f46946
nazarrachunn <nazarrachun@gmail.com>
2021-12-02 01:30:38 +0200

    Zzz

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -5,8 +5,16 @@ class Greed
       if number == nil
       raise GreedError
       end
-      total_score = 0
+      if number.include?(5)
+        return 50
+      elsif number.count(1)/3 == 1
+        return 1000
+      elsif number.include?(1)
+        return 100
+      end
+      total = 0
     end
 end
+
 class GreedError < StandardError
 end
