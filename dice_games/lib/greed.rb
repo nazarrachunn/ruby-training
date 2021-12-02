63d9660182526fcdcfd1cac0f82642235f8e1a92
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2021-12-02 03:35:29 +0200

    Greed (#50)
    * understand how it works
    
    * couple done
    
    * 17 left
    
    * ...
    
    * bad way
    
    * Zzz
    
    * some result
    
    * 8 last
    
    * 7 lats
    
    * 6
    
    * bad way
    
    Co-authored-by: nazarrachunn <nazarrachun@gmail.com>

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -5,6 +5,28 @@ class Greed
       if number == nil
       raise GreedError
       end
+
+          if number.count(5)/3 == 1 
+        return 500 
+      elsif number.count(1)*3 == 9
+        return 1000
+      elsif number.count(1)-1 == 0
+        return 100
+      elsif number.count(2)/3 == 1
+        return 200
+      elsif number.count(3)/3 == 1
+        return 300
+      elsif number.count(4)/3 == 1
+        return 400
+      elsif number.count(5)/1 == 1
+        return 50
+      elsif number.count(6)/3 == 1
+        return 600
+      elsif number.include?(1 && 1 && 5 && 5)
+        return 300
+      elsif number.count(1)/5 == 1
+        return 1200
+      end  
       total_score = 0
     end
 end
