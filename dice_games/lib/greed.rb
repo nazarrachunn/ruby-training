de15ac6129ac908fef7fede97b199a0f2b3d0722
nazarrachunn <nazarrachun@gmail.com>
2021-12-02 02:38:33 +0200

    6

--- dice_games/lib/greed.rb
+++ dice_games/lib/greed.rb
@@ -7,7 +7,7 @@ class Greed
       end
       if number.count(5)/3 == 1 
         return 500 
-      elsif number.count(1)/3 == 1
+      elsif number.count(1)*3 == 9
         return 1000
       elsif number.count(1)/1 == 1
         return 100
@@ -21,8 +21,10 @@ class Greed
         return 50
       elsif number.count(6)/3 == 1
         return 600
-      elsif number.include?(1&&1&&5&&5)
+      elsif number.include?(1 && 1 && 5 && 5)
         return 300
+      elsif number.count(1)/5 == 1
+        return 1200
       end
       total = 0
     end
