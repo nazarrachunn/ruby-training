6e163d66b0f3a56d5507ad253816d600e78499a7
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2021-12-07 09:24:37 +0200

    Bottles exercise (#57)
    * finish
    
    * test
    
    * sing typical verse
    
    Co-authored-by: nazarrachunn <nazarrachun@gmail.com>

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class Bottles
+    def verse (song)
+        song = "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n" 
+    end
+
+    def verses(song)
+    end
+    
+    def sing(song)
+    end
 end
+    
