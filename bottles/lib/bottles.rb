9e40f7a9c65cc9c67a9e76ad58f3294771d8a5e2
nazarrachunn <nazarrachun@gmail.com>
2021-12-08 14:38:06 +0200

    first verse pass

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,20 +1,10 @@
 # frozen_string_literal: true
 
 class Bottles
-    def verse (song)
-        bottles_before = 8
-        bottles_after = (bottles_before - 1)
-        song_pieces = ["#{bottles_before} bottles of beer on the wall, ", "#{bottles_before} bottles of beer.", "\nTake one down and pass it around, ","#{bottles_after} bottles of beer on the wall.\n"]
-        x = "/n"
-        song_pieces.join
-    end
-
-    def verses
-
-    end
-    
-    def sing
-
-    end
+    def verse(number)
+    "#{number} bottles of beer on the wall, " +
+    "#{number} bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "#{number -1} bottles of beer on the wall.\n"
+  end
 end
-    
