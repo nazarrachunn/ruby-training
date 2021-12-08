b545c1aa5599465d1dfb6bcda57d4d1b61ea1ed5
nazarrachunn <nazarrachun@gmail.com>
2021-12-08 21:07:59 +0200

    add zero method and change some part of code

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,10 +2,12 @@
 
 class Bottles
     def verse(number)
+       
         lesser_number = number - 1
-        "#{number} bottle#{change} of beer on the wall, 
-        #{number} bottle#{change} of beer.\nTake #{change_word} down and pass it around, 
-        #{lesser_number} bottles of beer on the wall.\n"
+        "#{number} bottle#{change(number)} of beer on the wall, " +
+        "#{number} bottle#{change(number)} of beer.\nTake " +
+        "#{change_word(number)} down and pass it around, " +
+        "#{zero(lesser_number)} bottle#{change(lesser_number)} of beer on the wall.\n"
     end
     def change(number)
         if number == 1
@@ -13,14 +15,20 @@ class Bottles
         else
             "s"
         end
+    end
     def change_word(number)
         if number == 1
             "it"
         else
             "one"
-
-
+        end 
     end
-    end 
+    def zero(lesser_number)
+        if lesser_number == 0
+            "no more"
+        else
+            lesser_number
+        end
+    end
 end
 
