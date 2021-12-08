1795baf466371a44693ab179ca94a0461285ff8c
nazarrachunn <nazarrachun@gmail.com>
2021-12-08 16:38:53 +0200

    add change method

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,16 +2,16 @@
 
 class Bottles
     def verse(number)
-        if number == 0
-        verse = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-        elsif number == 8
-        verse = "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
-        elsif number == 3
-            verse = "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
-        elsif number == 1
-            verse = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-        elsif number == 2
-            verse = "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+        lesser_number = number - 1
+        "#{number} bottle#{change} of beer on the wall, #{number} bottle#{change} of beer.\nTake one down and pass it around, #{lesser_number} bottles of beer on the wall.\n"
+    end
+    def change(number)
+        if number == 1
+            ""
+        else
+            "s"
         end
-  end
+
+    end 
 end
+
