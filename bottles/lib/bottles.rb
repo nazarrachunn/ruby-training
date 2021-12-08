e022c1894dccc1a0fd4a4c692dc7999b5470fb9a
nazarrachunn <nazarrachun@gmail.com>
2021-12-08 15:01:18 +0200

    sing typical verses and 'No more bottles'

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,9 +2,12 @@
 
 class Bottles
     def verse(number)
-    "#{number} bottles of beer on the wall, " +
-    "#{number} bottles of beer.\n" +
-    "Take one down and pass it around, " +
-    "#{number -1} bottles of beer on the wall.\n"
+        if number == 0
+        verse = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+        elsif number == 8
+        verse = "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
+        elsif number == 3
+            verse = "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
+        end
   end
 end
