d3da9a59ccff9cf872b763c33eed60bcc4936488
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2021-12-16 15:49:40 +0200

    create method result for 0 and 8 digits (#66)

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -1,5 +1,31 @@
 # frozen_string_literal: true
 
 class LCD
-  # Write your code in here
+  def result(digit)
+    if digit == 0
+      upper_part_of_digit(digit) + middle_part_of_digit(digit) + lower_part_of_digit(digit)  
+    else expected = <<~SQUIGGLY_HEREDOC
+      _ 
+     |_|
+     |_|
+
+   SQUIGGLY_HEREDOC
+      
+    end
+  end
+
+  def upper_part_of_digit(digit) 
+        " _\n"
+  end
+
+  def middle_part_of_digit(digit)
+    "|_|\n"
 end
+
+  def lower_part_of_digit(digit)
+        "|_| "
+  end
+
+end
+lcd = LCD.new
+puts lcd.result(0)
