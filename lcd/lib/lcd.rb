13ad5b2c6abd572382ed00de0b2b3815871da181
nazarrachunn <nazarrachun@gmail.com>
2022-01-04 00:52:47 +0200

    added new methodsfor the rest of numbers

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -7,10 +7,24 @@ class LCD
       eight.values.join
     when 0
       zero.values.join
-    else 6
+    when 6
       six.values.join
-
+    when 1
+      one.values.join
+    when 2
+      two.values.join
+    when 3
+      three.values.join
+    when 4
+      four.values.join
+    when 5
+      five.values.join
+    when 7
+      seven.values.join
+    else 9
+      nine.values.join
     end
+
   end
 
   def zero
@@ -36,15 +50,63 @@ class LCD
       lower_part_of_digit: "|_| "
     }
   end
-  
-  def eight
+
+  def one
+    {
+      upper_part_of_digit: " \n",
+      middle_part_of_digit: " |\n",
+      lower_part_of_digit: " | "
+    }
+  end
+
+  def two
+    {
+      upper_part_of_digit: " _\n",
+      middle_part_of_digit: " _|\n",
+      lower_part_of_digit: "|_"
+    }
+  end
+
+  def three
+    {
+      upper_part_of_digit: " _\n",
+      middle_part_of_digit: " _|\n",
+      lower_part_of_digit: " _| "
+    }
+  end
+
+  def four
+    {
+      upper_part_of_digit: " \n",
+      middle_part_of_digit: "|_|\n",
+      lower_part_of_digit: "  |"
+    }
+  end
+
+  def five
+    {
+      upper_part_of_digit: " _\n",
+      middle_part_of_digit: "|_\n",
+      lower_part_of_digit: " _|"
+    }
+  end
+
+  def seven
+    {
+      upper_part_of_digit: "_\n",
+      middle_part_of_digit: " |\n",
+      lower_part_of_digit: " |"
+    }
+  end
+
+  def nine
     {
       upper_part_of_digit: " _\n",
       middle_part_of_digit: "|_|\n",
-      lower_part_of_digit: "|_| "
+      lower_part_of_digit: " _| "
     }
   end
 end
 
 lcd = LCD.new
-puts lcd.result(4)
+puts lcd.result(9)
