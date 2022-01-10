53d4c0feeba20b893a0e67227fa5d3b05ef60a91
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2022-01-10 02:07:30 +0200

    numbers passed (#74)

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -1,52 +1,27 @@
 # frozen_string_literal: true
 
 class LCD
-  attr_reader :digit, :lcd_states, :lcd_display_data
-
-  def initialize
-    @digit = digit
-    @lcd_states = %w[HORIZONTAL VERTICAL HORIZONTAL VERTICAL HORIZONTAL]
-    @lcd_display_data = {
-      0 => [1, 3, 2, 3, 1],
-      1 => [2, 5, 2, 5, 2],
-      2 => [1, 5, 1, 4, 1],
-      3 => [1, 5, 1, 5, 1],
-      4 => [2, 3, 1, 5, 2],
-      5 => [1, 4, 1, 5, 1],
-      6 => [1, 4, 1, 4, 1],
-      7 => [1, 5, 2, 5, 2],
-      8 => [1, 3, 1, 3, 1],
-      9 => [1, 3, 1, 5, 1]
+  def render(string)
+    config = {
+      "0" => [' _', '| |', '|_|'],
+      "1" => ['', '  |', '  |'],
+      "2" => [' _', ' _|', '|_'],
+      "3" => [' _', ' _|', ' _|'],
+      "4" => ['  ', '|_|', '  |'],
+      "5" => [' _', '|_', ' _|'],
+      "6" => [' _', '|_ ', '|_|',],
+      "7" => [' _', '  |', '  |'],
+      "8" => [' _', '|_|', '|_|'],
+      "9" => [' _', '|_|', ' _|']
     }
+    array = string.split('')
+    number_of_lines = 5
+
+    result = (0..number_of_lines - 1).inject("") { |final, num| final + "#{array.inject("") { |line, char| line + "#{config[char][num]}\t" }}\n" }
   end
-    def render(digit)
-      if digit == 1
-      lcd_display_data.values.join
-      else
-      lcd_display_data.keys.join
-      end
-    end
-
-    def horizontal_segment(position)
-      case position
-      when 1
-        " " + "_"
-      when 2
-        " " + " "
-      end
-    end
-
-    def vertical_element(position)
-      case position
-      when 3
-        " " + "| |"
-      when 4
-        " " + " |"
-      when 5
-        " " + "| "
-      end
-    end
+  lcd = LCD.new
+  puts lcd.render('1273')
+  
 end
-
-lcd = LCD.new
-puts lcd.render(1)
+  
+  
