069ada2693f466f1db464607d607ec094cb918a6
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2022-01-13 00:12:28 +0200

    Proxy1 (#80)
    * created initial proxy_object
    
    * allow the proxy object to be initialize with any object

--- proxy/lib/proxy.rb
+++ proxy/lib/proxy.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 class Proxy
+  
     def initialize(proxy_object)
         @proxy_object = proxy_object
+    end
+
+    def method_missing(method_name, *args, &block)
+        if @proxy_object.respond_to?(method_name)
+          @proxy_object.send(method_name, *args, &block)
+        else
+          super(method_name, *args, &block)
+        end
     end
 end
 
