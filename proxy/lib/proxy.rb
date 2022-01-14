b1bdcba9127a5b07d3c0b66d7bab7b4b09dca61b
nazarrachunn <nazarrachun@gmail.com>
2022-01-14 19:15:46 +0200

    vscode debug extantion added

--- proxy/lib/proxy.rb
+++ proxy/lib/proxy.rb
@@ -13,5 +13,18 @@ class Proxy
           super(method_name, *args, &block)
         end
     end
+
+    def respond_to?(method_name, include_private = false)
+      proxy_method_name = @proxy_object.methods
+      if proxy_method_name.include?(method_name)
+        true
+      else
+        super(methodd_name, include_private)
+      end
+    end
+
+    def called?
+      messages.include?(method_name)
+    end
 end
 
