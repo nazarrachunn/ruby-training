48a8f9dacae897b237b021633d9c991a8d4d214a
nazarrachunn <nazarrachun@gmail.com>
2022-01-16 22:36:05 +0200

    assigned messages method for storing messages

--- proxy/lib/proxy.rb
+++ proxy/lib/proxy.rb
@@ -1,35 +1,34 @@
 # frozen_string_literal: true
 
 class Proxy
-  
-    def initialize(proxy_object)
-        @proxy_object = proxy_object
-        @messages = []
-    end
+  def initialize(proxy_object)
+    @proxy_object = proxy_object
+    @messages = []
+  end
 
-    def method_missing(method_name, *args, &block)
-        if @proxy_object.respond_to?(method_name)
-          @proxy_object.send(method_name, *args, &block)
-        else
-          super(method_name, *args, &block)
-        end
+  def method_missing(method_name, *args, &block)
+    if @proxy_object.respond_to?(method_name)
+      @messages.push(method_name)
+      @proxy_object.send(method_name, *args, &block)
+    else
+      super(method_name, *args, &block)
     end
+  end
 
-    def respond_to?(method_name, include_private = false)
-      proxy_method_name = @proxy_object.methods
-      if proxy_method_name.include?(method_name)
-        true
-      else
-        super(method_name, include_private)
-      end
+  def respond_to?(method_name, include_private: false)
+    proxy_method_name = @proxy_object.methods
+    if proxy_method_name.include?(method_name)
+      true
+    else
+      super(method_name, include_private)
     end
+  end
 
-    def called?(msg)
-      @messages.include?(msg)
-    end
+  def called?(msg)
+    @messages.include?(msg)
+  end
 
-    def  number_of_times_called(msg)
-      @messages.count(msg)
-    end
+  def number_of_times_called(msg)
+    @messages.count(msg)
+  end
 end
-
