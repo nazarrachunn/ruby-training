913e9fc85dfb1a4173c6e8795d162c8a26c71055
nazarrachunn <nazarrachun@gmail.com>
2022-01-20 22:07:42 +0200

    exercise_1 passed

--- site_prism/exercise_1.rb
+++ site_prism/exercise_1.rb
@@ -19,14 +19,17 @@ page.load
 # the keys which is the name of the key preceded by a colon
 
 # 1) Find the number box on the page
-
+number_box = page.number_box
 # 2) Type in any odd 5 digit number i.e. 12345
-
+number_box.send_keys('12345')
 # 3) Using the up arrow, increase the number by 5
-
+5.times { number_box.send_keys(:up) }
 # 4) Find out the new number that is in the box
-
+number = number_box['value']
 # 5) Delete the number and replace it with a number that is exactly half of that
+5.times { number_box.send_keys(:backspace) }
+new_number = number.to_i / 2
+number_box.send_keys(new_number.to_s)
 
 # Wait and close
 sleep 2
